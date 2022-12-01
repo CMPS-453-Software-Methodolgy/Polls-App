b38777ab1719e1a0611f15c16ee2e99777edadc3
Seshagiri Prabhu <seshagiri.narasimha1@louisiana.edu>
2022-12-01 07:49:06 -0600

    Attempting to use pip and python instead of python3 and pip3 in the cypress job

--- .gitlab/cypress_before_install.sh
+++ .gitlab/cypress_before_install.sh
@@ -1,11 +1,11 @@
 npm ci >/dev/null
 npm i cypress --save-dev >/dev/null
 apt-get update >/dev/null
-apt-get install -y python3 python3-dev python3-pip >/dev/null
-pip3 install -q virtualenv
-virtualenv venv >/dev/null
+apt-get install -y python3 python3-dev python3-pip
+pip install virtualenv
+virtualenv venv
 source venv/bin/activate
-pip3 install --upgrade -q pip
-pip3 install --no-cache-dir -qr requirements.txt
-python3 manage.py migrate >/dev/null
-python3 manage.py collectstatic --no-input >/dev/null
+pip install --upgrade -q pip
+pip install --no-cache-dir -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --no-input >/dev/null
