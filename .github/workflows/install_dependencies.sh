75a72f29960af77f7343c858efa640aa475bcf01
Seshagiri Prabhu <seshagiri.narasimha1@louisiana.edu>
2024-04-23 08:04:58 -0500

    Attempting to use matrix.version

--- .github/workflows/install_dependencies.sh
+++
@@ -1,7 +0,0 @@
-python -m pip install --upgrade -q pip
-pip install --no-cache-dir -qr requirements.txt
-python manage.py migrate >/dev/null
-python manage.py collectstatic --no-input >/dev/null
-rm -rf staticfiles/fontawesomefree/js-packages
-rm -rf staticfiles/fontawesomefree/svgs
-du -sh staticfiles/fontawesomefree/metadata
